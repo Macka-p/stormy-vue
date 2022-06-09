fd25bba0c2a1a66c68c3ba5f50b2b348534c3591
Macka-p <pierre.viers@hotmail.fr>
2022-06-09 09:17:31 +0200

    added User class, added component for profile, blog article, login fo… …rm, profile-editor, added vuex, added news service, added user service, added views , added packages, updated readme

--- src/http-common.js
+++ src/http-common.js
@@ -4,5 +4,11 @@ export default axios.create({
     baseURL: "https://api-adresse.data.gouv.fr",
     headers: {
         "Content-type": "application/json",
-            }
+    }
 });
+
+// this.results = response?.data?.features;
+// if(this.results !== null && this.results !== undefined && this.results.length > 0){
+//      return;
+// }
+// throw new Error('rien a faire ici');
